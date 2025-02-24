38279096fcf437de27521586d91ccb6b1231fff3
wsevolves <165700713+wsevolves@users.noreply.github.com>
2025-02-25 00:54:48 +0530

    Update socket.js

--- socket.js
+++ socket.js
@@ -3,7 +3,7 @@ const { Server } = require("socket.io");
 function setupSocket(server) {
   const io = new Server(server, {
     cors: {
-      origin: ["http://localhost:3000", "http://127.0.0.1:5500"],
+      origin: ["http://localhost:3000", "http://127.0.0.1:5500",https://pbpartner.co.in],
       methods: ["GET", "POST", "PUT", "DELETE"],
     },
   });
