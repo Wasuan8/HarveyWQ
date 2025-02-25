712e36fc2ac4b6647065e00e447f9787751da2e1
wsevolves <165700713+wsevolves@users.noreply.github.com>
2025-02-25 17:17:20 +0530

    Update donor.js

--- models/donor.js
+++ models/donor.js
@@ -5,6 +5,7 @@ const donorSchema = new mongoose.Schema({
   number: { type: String, required: true },
   email: { type: String, required: true },
   category: { type: String, required: true },
+  amount: { type: Number, required: true },
   paymentMethod: { type: String, required: true },
   paymentRefId: { type: String, required: true },
   paymentDate: { type: Date, default: Date.now },
